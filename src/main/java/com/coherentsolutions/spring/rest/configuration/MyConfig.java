0882b0e1a412b88b4ec1ef694a24cc778d47ef72
iWaraxe <121664848+MariaESLIW@users.noreply.github.com>
2024-05-06 11:44:01 +0200

    Initial Config

--- src/main/java/com/coherentsolutions/spring/rest/configuration/MyConfig.java
+++ src/main/java/com/coherentsolutions/spring/rest/configuration/MyConfig.java
@@ -1,2 +1,12 @@
-package com.coherentsolutions.spring.rest.configuration;public class MyConfig {
+package com.coherentsolutions.spring.rest.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = "com.coherentsolutions.spring.rest")
+@EnableWebMvc
+public class MyConfig {
+
 }
